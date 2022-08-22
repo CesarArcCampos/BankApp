8d752e64a4a81742369e38b2a1143665abc1c901
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-08-22 16:51:57 +0100

    Add Account class to model package with ManyToOne annotation

---
+++ src/main/java/com/mycompany/bankApp/model/Account.java
@@ -0,0 +1,34 @@
+package com.mycompany.bankApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int amount;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+
+    @ManyToOne
+    private Customer customer;
+}
