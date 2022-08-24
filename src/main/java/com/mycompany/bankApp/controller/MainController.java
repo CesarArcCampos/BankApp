6775cae55a5cf1a458393dd9bb15ce0e9932a5f3
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-08-24 12:21:48 +0100

    Add customer form html and respective handler to main controller. Handling the save option to create new customer to db

--- src/main/java/com/mycompany/bankApp/controller/MainController.java
+++ src/main/java/com/mycompany/bankApp/controller/MainController.java
@@ -1,13 +1,33 @@
 package com.mycompany.bankApp.controller;
 
+import com.mycompany.bankApp.model.Customer;
+import com.mycompany.bankApp.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    CustomerService customerService;
+
     @GetMapping("/login")
     public String login() {
         return "index";
+    }
+
+    @GetMapping("/customer/new")
+    public String newCustomerForm(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/customer/save")
+    public String saveNewCustomer(Customer customer) {
+        customerService.createNewCustomer(customer);
+        return "redirect:/login";
     }
 }
