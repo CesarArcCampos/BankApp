8737a9b509b17129c9927d23f0bac516c8e76322
Cesar.Arc.Campos <cesar.arc.campos@gmail.com>
2022-08-26 16:58:50 +0100

    Add log repository

---
+++ src/main/java/com/mycompany/bankApp/repository/LogRepo.java
@@ -0,0 +1,11 @@
+package com.mycompany.bankApp.repository;
+
+import com.mycompany.bankApp.model.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface LogRepo extends JpaRepository<Log, Long> {
+    
+}
